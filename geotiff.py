e9653bebd043d6904474e8d6ec989992ac030d45
Dennis Sergeev <dennis.sergeev@gmail.com>
2015-12-27 23:03:43 +0300

    geotiff: automatic retrieval of lon0/lat0 from a projection string

--- geotiff.py
+++ geotiff.py
@@ -22,8 +22,31 @@ def read_gtiff(fname):
     xx, yy = np.meshgrid(x, y)
     
     # Set a map using the Cassini-Soldner projection
-    sat_m = Basemap(projection='cass',lon_0=10.,lat_0=74, width=nx, height=ny)
+    lon0, lat0 = get_lonlat_from_proj_str(gtif.GetProjection())
+    sat_m = Basemap(projection='cass',lon_0=lon0,lat_0=lat0, width=nx, height=ny)
     #Convert image coordinates from meters to lon/lat
     lons, lats = sat_m(xx,yy,inverse=True)
 
     return lons, lats, arr
+
+
+def get_lonlat_from_proj_str(proj_str, lon_key='central_meridian',
+                                       lat_key='latitude_of_origin'):
+    lon0 = _get_numvalue_by_str(proj_str, lon_key)
+    lat0 = _get_numvalue_by_str(proj_str, lat_key)
+
+    return lon0, lat0    
+
+
+def _get_numvalue_by_str(s, key):
+    ibeg = s.find(key)
+    if s[ibeg-2] != '[':
+        warnings.warn('Uncertain results!')
+
+    iend = s[ibeg:].find(']')
+    str_till_next_bracket = s[ibeg:ibeg+iend]
+    found = re.findall(r"[+-]?\d+(?:\.\d+)?", str_till_next_bracket)
+    if len(found) > 0:
+        return float(found[0])
+    else:
+        warnings.warn('Not found any numeric values')
